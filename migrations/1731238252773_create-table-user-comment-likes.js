77490723677d4b13b8bd5ec755f23b44cd7b79aa
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-11-10 19:00:01 +0700

    add nginx config file

---
+++ migrations/1731238252773_create-table-user-comment-likes.js
@@ -0,0 +1,51 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("user_comment_likes", {
+    user_comment_likes_id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    user_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    comment_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint("user_comment_likes", "unique_user_id_comment_id", {
+    unique: ["user_id", "comment_id"],
+  });
+
+  pgm.addConstraint(
+    "user_comment_likes",
+    "fk_user_comment_likes.user_id_users.id",
+    "FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE"
+  );
+
+  pgm.addConstraint(
+    "user_comment_likes",
+    "fk_user_comment_likes.comment_id_comments.id",
+    "FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE"
+  );
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("user_comment_likes");
+};
